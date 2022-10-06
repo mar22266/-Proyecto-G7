b4fe7f39ebb221d51024d5728ddf751ce44dbcc9
nel-eleven11 <104164145+nel-eleven11@users.noreply.github.com>
2022-10-06 09:53:58 -0600

    Update principal.java
    commit de mate

--- principal.java
+++ principal.java
@@ -25,34 +25,101 @@ public class principal {
 
                 //MATE
                 case 2: {
-
-                    m.sumar();
-                    m.restar();
-                    int e = 0;
-                    String res1 = "";
-                    System.out.println("\nResuelva los siguientes ejercicios:");
-                    System.out.println("\n");
-                    for (int i = 0; i <5; i++){
-                        System.out.println("\n"+m.getEjercicios().get(i));
-                        res1 = teclado.nextLine();
-                        String resp = m.getRespuestas().get(i);
-                        if(res1.equals(resp)){
-                            System.out.println("Buen trabajo"); 
-                        }
-                        else{
-                            System.out.println("Respuesta incorrecta");
-                            e++;
-                            m.setErrores(e);
-                        }
-                    }
-                    System.out.println("Respuestas correctas: "+(5-e));
-                    System.out.println("Respuestas incorrectas: "+(e));
-                    System.out.println("\n");
-                    
+                    String menuMate = "1. Repasar \n2. Sumas \n3. Restas \n4. Regresar a menu principal";
+                    int opcionMate = 1;
                     System.out.println(menuMate);
-                    System.out.println("Ingrese una opción del menú: ");
+                    System.out.println("Ingrese la opción que desea: ");
                     opcionMate = teclado.nextInt();
                     teclado.nextLine();
+                    
+                    while (opcionMate < 4 && opcionMate>=1 ){
+                        switch(opcionMate){
+
+                            case 1:{
+                                System.out.println("\n");
+                                System.out.println("\nLas sumas y restas son las primeras operaciones matemáticas que aprendemos. ");
+                                System.out.println("\nAntes de aprender a sumar y restar, vamos a explicar qué es una suma y qué es una resta:");
+                                System.out.println("\nSumar: Sumar es juntar dos o más cosas en un grupo, para saber cuántas hay en total");
+                                System.out.println("\nRestar: Restar es quitar una cierta cantidad a otra que ya teníamos");
+                                System.out.println("\nEjemplo de suma: ");
+                                System.out.println("\nSi tengo 2 manzanas verdes y 3 manzanas rojas, y quiero saber cuántas manzanas tengo"+ 
+                                "en total, junto todas las manzanas en un solo cesto y las cuento: tengo 5 manzanas en total, por lo tanto 2 + 3  es igual a 5.");
+                                System.out.println("\nEjemplo de resta: ");
+                                System.out.println("\nSi tengo 5 manzanas en una cesta, y quito 2, dentro de la cesta me quedarán 3 manzanas,"+
+                                 "es decir que 5 menos 2 es igual a 3.");
+
+                                System.out.println(menuMate);
+                                System.out.println("Ingrese una opción del menú: ");
+                                opcionMate = teclado.nextInt();
+                                teclado.nextLine();
+                                //sd
+
+                                break;
+                            }
+                            case 2:{
+                                m.sumar();
+                                m.restar();
+                                int e = 0;
+                                String res1 = "";
+                                System.out.println("\nResuelva los siguientes ejercicios:");
+                                System.out.println("\n");
+                                for (int i = 0; i <5; i++){
+                                    System.out.println("\n"+m.getEjercicios().get(i));
+                                    res1 = teclado.nextLine();
+                                    String resp = m.getRespuestas().get(i);
+                                    if(res1.equals(resp)){
+                                        System.out.println("Buen trabajo"); 
+                                    }
+                                    else{
+                                        System.out.println("Respuesta incorrecta");
+                                        e++;
+                                        m.setErrores(e);
+                                    }
+                                }
+                                System.out.println("Respuestas correctas: "+(5-e));
+                                System.out.println("Respuestas incorrectas: "+(e));
+                                System.out.println("\n");
+
+
+                                System.out.println(menuMate);
+                                System.out.println("Ingrese una opción del menú: ");
+                                opcionMate = teclado.nextInt();
+                                teclado.nextLine();
+                                break;
+                            }
+                            case 3:{
+                                int e = 0;
+                                m.sumar();
+                                m.restar();
+                                String res2 = "";
+                                System.out.println("\nResuelva los siguientes ejercicios:");
+                                System.out.println("\n");
+                                for (int i = 5; i <10; i++){
+                                    System.out.println("\n"+m.getEjercicios().get(i));
+                                    String resp2 = m.getRespuestas().get(i);
+                                    res2 = teclado.nextLine();
+                                    if(res2.equals(resp2)){
+                                        System.out.println("Buen trabajo");
+                                    }
+                                    else{
+                                        System.out.println("Respuesta incorrecta");
+                                        e++;
+                                        m.setErrores(e);
+                                    }
+                                }
+                                System.out.println("Respuestas correctas: "+(5-e));
+                                System.out.println("Respuestas incorrectas: "+(e));
+                                System.out.println("\n");
+                                System.out.println(menuMate);
+                                System.out.println("Ingrese una opción del menú: ");
+                                opcionMate = teclado.nextInt();
+                                teclado.nextLine();
+                                break;
+                            }
+
+                        }
+                        
+                    }
                     break;
                 }
 
